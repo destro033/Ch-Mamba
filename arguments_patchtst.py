eb66ec77f71429f81b7e9425a0408198afe292c1
Phivos <savvaphivos@gmail.com>
2026-04-26 15:01:11 +0300

    Update arguments_patchtst.py

--- arguments_patchtst.py
+++ arguments_patchtst.py
@@ -1,18 +1,17 @@
 class Config:
-    task_name = "long_term_forecast"
-    seq_len = 96
-    pred_len = 96
-    enc_in = 3   # number of variables
-    d_model = 128
-    n_heads = 4
-    e_layers = 2
-    d_ff = 512
-    dropout = 0.1
+    task_name = "long_term_forecast" #default
+    seq_len = 96    #lookback window
+    pred_len = 96   #forecast length
+    enc_in = 3      #number of variables in the data
+    d_model = 128   #dimension of the model 
+    n_heads = 4     
+    e_layers = 2    #how many blocks 
+    d_ff = 512      #inner dimension
+    dropout = 0.1  
     factor = 1
-    activation = "gelu"
-    e_layers = 2 
-    patch_len = 16
-    stride = 8
+    activation = "gelu" #activation function
+    patch_len = 16  #patch length
+    stride = 8      #window for patching
 
     #training settings 
     batch_size = 32
